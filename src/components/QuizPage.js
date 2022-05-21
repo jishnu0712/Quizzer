4a830c120337f3da68e19cea8e65864108b42f66
Jishnu <jishnu.dev.roy@gmail.com>
2022-05-22 01:51:14 +0530

    working check answers

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -2,15 +2,19 @@ import React from "react";
 import Quizz from "./Quizz";
 import { nanoid } from 'nanoid'
 
-const URL = `https://opentdb.com/api.php?amount=5&type=multiple`;
+
+const URL =
+    `https://opentdb.com/api.php?amount=5&type=multiple`;
 
 let FilledQuizz = [];
+
 
 export default function QuizPage() {
     const [userAnswers, setUserAnswers] = React.useState({});
     const [questions, setQuestions] = React.useState([]);
-console.log(userAnswers);
+
     function handleOptionClick(questionID, ans) {
+
         setUserAnswers(prev => (
             {
                 ...prev,
@@ -56,9 +60,10 @@ console.log(userAnswers);
     if (questions.length > 0) {
         FilledQuizz = questions.map(ele => (<Quizz
             key={ele.id}
+
             id={ele.id}
-            question={ele.question}
             handleOptionClick={handleOptionClick}
+            question={ele.question}
             userAnswers={userAnswers}
             correct_answer={ele.correct_answer}
             answers={[...ele.incorrect_answers, ele.correct_answer]}
--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -7,43 +7,44 @@ const correctOption = { backgroundColor: '#94D7A2' }
 const incorrectOption = { backgroundColor: '#F8BCBC' }
 
 export default function Quizz(props) {
-    const [quizzContent, setQuizzContent] = React.useState({ style: {}, selectedOption: '' });
-
     const questionID = props.id;
+
     function handleClick(ans) {
-        setQuizzContent({ style: selectedOptionStyle, selectedOption: ans });
         props.handleOptionClick(questionID, ans);
     }
 
-    // function getColor(ans) {
-    //     if (props.userAnswers.checkAnswer) { //check correct ans
-    //         if (questionID in props.userAnswers) {
-    //             if (ans === props.correct_answer) return correctOption;
-    //             return incorrectOption;
-    //         }
-    //         return {};
-    //     }//mark selection
-    //     else {
-    //         if (ans === props.userAnswers[questionID]) {
-    //             return selectedOptionStyle;
-    //         }
-    //         else return {};
-    //     }
-    // }
-
 
     const optionsArr = props.answers.map(ele => {
-        return (ele === quizzContent.selectedOption) ?
-            <Options
+        if (ele === props.userAnswers[questionID]) {
+            if (props.userAnswers.checkAnswer) {
+                if (ele === props.correct_answer) {
+                    return (<Options
+                        optionText={ele}
+                        handleClick={handleClick}
+                        styles={correctOption}
+                    />)
+                } else {
+                    return (<Options
+                        optionText={ele}
+                        handleClick={handleClick}
+                        styles={incorrectOption}
+                    />)
+                }
+
+            }
+            return (<Options
                 optionText={ele}
                 handleClick={handleClick}
-                styles={quizzContent.style}
-            /> :
-            <Options
+                styles={selectedOptionStyle}
+            />)
+        }
+        else {
+            return (<Options
                 optionText={ele}
                 handleClick={handleClick}
                 styles={{}}
-            />
+            />)
+        }
     })
 
     return (
